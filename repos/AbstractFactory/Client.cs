537f815a03d480d0077960be9ff22718134377ce
tomas <tomas.frankl@gmail.com>
2020-01-06 13:15:12 +0100

    AbstractFactory - client

---
+++ repos/AbstractFactory/Client.cs
@@ -0,0 +1,21 @@
+﻿namespace AbstractFactory
+{
+    class Client
+    {
+        IShapeFactory factory;
+
+        public Client(IShapeFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        public void Use()
+        {
+            var circle = factory.CreateCircle();
+            circle.UseCircle();
+
+            var square = factory.CreateSquare();
+            square.UseSquare();
+        }
+    }
+}
--- repos/AbstractFactory/Program.cs
+++ repos/AbstractFactory/Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AbstractFactory
+﻿namespace AbstractFactory
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var factory1 = new BlueFactory();
+            var client1 = new Client(factory1);
+            client1.Use();
+
+            var factory2 = new RedFactory();
+            var client2 = new Client(factory2);
+            client2.Use();
         }
     }
 }
